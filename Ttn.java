3307e76e8c1fcdef78e81867b5acd91a036a4f2f
kapil rajoria <53654820+rajoriakapil456@users.noreply.github.com>
2022-11-06 11:19:33 +0530

    Add files via upload

---
+++ Ttn.java
@@ -0,0 +1,10 @@
+package com.Bootcamp.core.services;
+
+public interface Ttn {
+
+	public String getName();
+	public int getNoe();
+	public String getDesc();
+	public String getLoc();
+	public String getServ();
+}
